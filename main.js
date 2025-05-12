d47936bb0148aefaff6ec8dd3e42adff1c335398
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-05-12 17:56:50 +0100

    Implement input validation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 let title, author, pages, read; // for saving new book info
+let booksInLibrary; // use to prevent adding repeat books to library
 
 const booksSection = document.querySelector(".books");
 const dialog = document.querySelector("dialog");
@@ -12,12 +13,18 @@ const pagesInput = document.querySelector("#pages");
 const yesRadio = document.querySelector("#yes");
 const noRadio = document.querySelector("#no");
 
+const form = document.querySelector("form");
+const titleError = document.querySelector("#title + .error");
+const authorError = document.querySelector("#author + .error");
+const bookAvailableError = document.querySelector("fieldset + .error");
+const pageError = document.querySelector("#pages + .error");
+
 // clicking outside dialog box closes it
-dialog.addEventListener("click", (event) => {
-  if (event.target === dialog) {
-    dialog.close();
-  }
-});
+// dialog.addEventListener("click", (event) => {
+//   if (event.target === dialog) {
+//     dialog.close();
+//   }
+// });
 
 newBookBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -27,33 +34,34 @@ closeBtn.addEventListener("click", () => {
   dialog.close();
 });
 
-submitBtn.addEventListener("click", () => {
-  // Don't add book with undefined attributes
-  if (title && author && pages) {
-    // Don't add previously added book
-    const booksInLibrary = myLibrary.filter(
-      (book) =>
-        book.title === title.toUpperCase() &&
-        book.author === author.toUpperCase()
-    );
-    if (booksInLibrary.length === 0) {
-      addBook(self.crypto.randomUUID(), title, author, pages, read);
-    }
+titleInput.addEventListener("input", (event) => {
+  if (titleInput.validity.valid) {
+    titleError.textContent = "";
+    titleError.className = "error";
+    title = event.target.value.trim();
+  } else {
+    showError();
   }
-  dialog.close();
-  displayBooks();
 });
 
-titleInput.addEventListener("change", (event) => {
-  title = event.target.value.trim();
+authorInput.addEventListener("input", (event) => {
+  if (authorInput.validity.valid) {
+    authorError.textContent = "";
+    authorError.className = "error";
+    author = event.target.value.trim();
+  } else {
+    showError();
+  }
 });
 
-authorInput.addEventListener("change", (event) => {
-  author = event.target.value.trim();
-});
-
-pagesInput.addEventListener("change", (event) => {
-  pages = parseInt(event.target.value);
+pagesInput.addEventListener("input", (event) => {
+  if (pagesInput.validity.valid) {
+    pageError.textContent = "";
+    pageError.className = "error";
+    pages = parseInt(event.target.value);
+  } else {
+    showError();
+  }
 });
 
 yesRadio.addEventListener("change", () => {
@@ -64,6 +72,48 @@ noRadio.addEventListener("change", () => {
   read = false;
 });
 
+form.addEventListener("submit", (event) => {
+  // Check if book has been previously added
+  booksInLibrary = myLibrary.filter(
+    (book) =>
+      book.title.toUpperCase() === title.toUpperCase() &&
+      book.author.toUpperCase() === author.toUpperCase()
+  );
+
+  if (
+    !titleInput.validity.valid ||
+    !authorInput.validity.valid ||
+    !pagesInput.validity.valid ||
+    booksInLibrary.length
+  ) {
+    showError();
+    event.preventDefault(); // prevent form submission
+  } else {
+    addBook(self.crypto.randomUUID(), title, author, pages, read);
+    dialog.close();
+    displayBooks();
+  }
+});
+
+function showError() {
+  if (titleInput.validity.valueMissing) {
+    titleError.textContent = "Title cannot be empty.";
+    titleError.className = "error active";
+  } else if (authorInput.validity.valueMissing) {
+    authorError.textContent = "Author cannot be empty.";
+    authorError.className = "error active";
+  } else if (
+    pagesInput.validity.valueMissing ||
+    pagesInput.validity.rangeUnderflow
+  ) {
+    pageError.textContent = "No. of pages cannot be empty nor 0.";
+    pageError.className = "error active";
+  } else if (booksInLibrary.length) {
+    bookAvailableError.textContent = "Book already in library.";
+    bookAvailableError.className = "error active";
+  }
+}
+
 // Using class
 class Book {
   constructor(id, title, author, pages, read) {
@@ -73,35 +123,17 @@ class Book {
     this.pages = pages;
     this.read = read;
   }
+
   info() {
     return this.read
       ? `${this.title} by ${this.author}, ${this.pages} pages, read it.`
       : `${this.title} by ${this.author}, ${this.pages} pages, yet to read it.`;
   }
+
   changeReadStatus() {
     return this.read ? (this.read = false) : (this.read = true);
   }
 }
-
-// Using object contstructor
-// function Book(id, title, author, pages, read) {
-//   if (!new.target) {
-//     throw Error("You must use the 'new' operator to call the constructor!");
-//   }
-//   this.id = id;
-//   this.title = title.toUpperCase();
-//   this.author = author.toUpperCase();
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     return this.read
-//       ? `${this.title} by ${this.author}, ${this.pages} pages, read it.`
-//       : `${this.title} by ${this.author}, ${this.pages} pages, yet to read it.`;
-//   };
-//   this.changeReadStatus = function () {
-//     return this.read ? (this.read = false) : (this.read = true);
-//   };
-// }
 
 function addBook(id, title, author, pages, read) {
   // take params, create a book then store it in the array
@@ -140,75 +172,4 @@ function displayBooks() {
   });
 }
 
-addBook(
-  self.crypto.randomUUID(),
-  "Dream Count",
-  "Chimamanda Ngozi Adichie",
-  400,
-  false
-);
-addBook(
-  self.crypto.randomUUID(),
-  "Purple Hibiscus",
-  "Chimamanda Ngozi Adichie",
-  200,
-  true
-);
-addBook(
-  self.crypto.randomUUID(),
-  "Half of a Yellow Sun",
-  "Chimamanda Ngozi Adichie",
-  600,
-  false
-);
-addBook(
-  self.crypto.randomUUID(),
-  "Americanah",
-  "Chimamanda Ngozi Adichie",
-  400,
-  false
-);
-addBook(
-  self.crypto.randomUUID(),
-  "The Thing Around Your Neck",
-  "Chimamanda Ngozi Adichie",
-  200,
-  true
-);
-addBook(
-  self.crypto.randomUUID(),
-  "The Accidental Tourist",
-  "Anne Tyler",
-  600,
-  false
-);
-addBook(
-  self.crypto.randomUUID(),
-  "Dinner at the Homesick Restaurant",
-  "Anne Tyler",
-  200,
-  true
-);
-addBook(
-  self.crypto.randomUUID(),
-  "A Good Man Is Hard to Find",
-  "Flannery O'Connor",
-  600,
-  false
-);
-addBook(
-  self.crypto.randomUUID(),
-  "Everything That Rises Must Converge",
-  "Flannery O'Connor",
-  600,
-  false
-);
-addBook(self.crypto.randomUUID(), "Arrow of God", "Chinua Achebe", 200, true);
-addBook(
-  self.crypto.randomUUID(),
-  "Things Fall Apart",
-  "Chinua Achebe",
-  600,
-  false
-);
 displayBooks();
